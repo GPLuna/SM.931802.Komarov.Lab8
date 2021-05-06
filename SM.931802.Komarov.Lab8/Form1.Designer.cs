defe70e862cfb7de182dfc33b4a2c6af2ec50ca6
GPLuna <GPLuna@mail.ru>
2021-05-06 16:14:36 +0700

    Добавьте файлы проекта.

---
+++ SM.931802.Komarov.Lab8/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SM._931802.Komarov.Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.YNButton = new System.Windows.Forms.Button();
+            this.BallButton = new System.Windows.Forms.Button();
+            this.LAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(122, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(541, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Стоит ли выйти из комнаты?";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // YNButton
+            // 
+            this.YNButton.Location = new System.Drawing.Point(122, 210);
+            this.YNButton.Name = "YNButton";
+            this.YNButton.Size = new System.Drawing.Size(158, 37);
+            this.YNButton.TabIndex = 1;
+            this.YNButton.Text = "Да\\Нет";
+            this.YNButton.UseVisualStyleBackColor = true;
+            this.YNButton.Click += new System.EventHandler(this.YNButton_Click);
+            // 
+            // BallButton
+            // 
+            this.BallButton.Location = new System.Drawing.Point(505, 210);
+            this.BallButton.Name = "BallButton";
+            this.BallButton.Size = new System.Drawing.Size(158, 37);
+            this.BallButton.TabIndex = 2;
+            this.BallButton.Text = "Шар предсказаний";
+            this.BallButton.UseVisualStyleBackColor = true;
+            this.BallButton.Click += new System.EventHandler(this.BallButton_Click);
+            // 
+            // LAnswer
+            // 
+            this.LAnswer.AutoSize = true;
+            this.LAnswer.Location = new System.Drawing.Point(119, 276);
+            this.LAnswer.Name = "LAnswer";
+            this.LAnswer.Size = new System.Drawing.Size(48, 17);
+            this.LAnswer.TabIndex = 3;
+            this.LAnswer.Text = "Ответ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LAnswer);
+            this.Controls.Add(this.BallButton);
+            this.Controls.Add(this.YNButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button YNButton;
+        private System.Windows.Forms.Button BallButton;
+        private System.Windows.Forms.Label LAnswer;
+    }
+}
+
---
+++ SM.931802.Komarov.Lab8/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM._931802.Komarov.Lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void YNButton_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            double a = r.NextDouble();
+            if (a < 0.5)
+            {
+                LAnswer.Text = "Да";
+            }
+            else
+            {
+                LAnswer.Text = "Нет";
+            }
+        }
+
+        private void BallButton_Click(object sender, EventArgs e)
+        {
+            string[] answers = { "Бесспорно", "Предрешено", "Мне кажется — «да»", "Вероятнее всего", "Спроси позже", "Сейчас нельзя предсказать", "Даже не думай", "Мой ответ — «нет»" };
+            int S = answers.Length;
+            double p = 1.0 / S;
+
+            Random r = new Random();
+            double A = r.NextDouble();
+            int ans_id = -1;    
+            do
+            {
+                A -= p;
+                ans_id++;
+            } while (A > 0);
+
+            LAnswer.Text = answers[ans_id];
+        }
+    }
+}
